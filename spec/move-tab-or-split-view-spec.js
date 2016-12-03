8229f626ff7a901eb074d63f93da15338cb32161
Vladimir Mikhaylovskiy <singingcode@gmail.com>
2016-12-02 20:57:58 -0800

    initial commit

---
+++ spec/move-tab-or-split-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import MoveTabOrSplitView from '../lib/move-tab-or-split-view';
+
+describe('MoveTabOrSplitView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
