ea93a280183c15d18cda363b9a5e005a1d7856f6
Vladimir Mikhaylovskiy <singingcode@gmail.com>
2016-12-02 21:11:53 -0800

    bugs fixed; moving functionality is done

--- spec/move-tab-or-split-view-spec.js
+++ spec/move-tab-or-split-view-spec.js
@@ -1,9 +1,3 @@
 'use babel';
 
 import MoveTabOrSplitView from '../lib/move-tab-or-split-view';
-
-describe('MoveTabOrSplitView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
